686741a1c6b8185d04c67d6e87df08738b3839c7
Nivedita6 <32298013+Nivedita6@users.noreply.github.com>
2023-05-21 19:12:50 +0530

    update:added filters in productlist page

---
+++ src/pages/ProductList/FilterPage.js
@@ -0,0 +1,50 @@
+import "./FilterPage.css";
+import { categories } from "../../backend/db/categories";
+
+export const FilterPage = () => {
+
+    const ratingStars = [1, 2, 3, 4];
+    return(
+        <>
+            <div className="filter-container">
+                <div className="filter-heading">
+                    <h4>FILTERS</h4>
+                    <input type="reset" value= "clear"/>
+                </div>
+
+                <div className="filter-price">
+                    <h4>PRICE RANGE</h4>
+                    <div className="flex-gap">
+                        <input type="range" min= "300" max = "2000" className="slider"/>
+                    </div>
+                </div>
+
+                <div className="filter-category">
+                    <h4>CATEGORY</h4>
+                    <div className="flex-gap">
+                        {categories.map(category => {
+                            return(
+                                <div key={category._id}>
+                                    <input type="checkbox" /> {category.categoryName}
+                                </div>
+                            )
+                        })}
+                    </div>
+                    
+                </div>
+
+                <div className="filter-rating">
+                    <h4>RATING</h4>
+                    <div className="flex-gap">
+                        {ratingStars.map(rating =>(
+                            <label key = {rating} className="select-input" >
+                                <input type = "radio" name = "rating" className="radio-input" />{rating} Stars and above
+                            </label>
+                        ) )}
+                    </div>
+                    
+                </div>
+            </div>
+        </>
+    )
+}
--- src/pages/ProductList/Products.js
+++ src/pages/ProductList/Products.js
@@ -1,5 +1,23 @@
+import { FilterPage } from "./FilterPage";
 import "./Products.css";
+import { products } from "../../backend/db/products";
+import { ProductCard } from "./ProductCard";
 
 export const Products = () => {
-    return <h1>This is products page.</h1>
+    return (
+        <div className="product-main-container">
+            <FilterPage />
+            <div className="product-list-container">
+                <div className="product-list-header">
+                    <h3>Show all products</h3>
+                    <div className="product-grid">
+                        {products.map(product => (
+                            <ProductCard key={product._id} product = {product} />
+                        ))}
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    )
 }
